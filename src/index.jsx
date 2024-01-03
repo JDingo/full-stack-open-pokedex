0e4765ce1d6df3d88570b4e570eedba1143cdc83
JDingo <45366094+JDingo@users.noreply.github.com>
2024-01-03 12:00:07 +0200

    Revert reactdom render and disable eslint

--- src/index.jsx
+++ src/index.jsx
@@ -1,6 +1,8 @@
+/* eslint-disable react/no-deprecated */
+
 import React from 'react'
-import { createRoot } from 'react-dom'
+import ReactDOM from 'react-dom'
 import App from './App'
 import './styles.css'
 
-createRoot(document.getElementById('app')).render(<App />)
+ReactDOM.render(<App />, document.getElementById('app'))
